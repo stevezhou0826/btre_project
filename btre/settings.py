5539c9ea29a493ec4f1fa8e01156a359a1e6263a
stevezhou0826 <zhousihao826@gmail.com>
2024-08-08 17:16:47 -0400

    8/8 6.38 completed

--- btre/settings.py
+++ btre/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
