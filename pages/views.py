5c0e13c34845f89b0c5c2fe5dd439b1ded0f5692
stevezhou0826 <zhousihao826@gmail.com>
2024-08-08 14:08:20 -0400

    8/8 5.27 completed

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
