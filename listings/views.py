54b8e86ffa8da1bea0ad9dcded84dbaae6e33ee3
stevezhou0826 <zhousihao826@gmail.com>
2024-08-09 10:57:00 -0400

    8/9 7.44 complete

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Listing
 
@@ -19,7 +19,13 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
