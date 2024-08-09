f57069c5f94dff7fb1f3876dd140c908601a1de4
stevezhou0826 <zhousihao826@gmail.com>
2024-08-09 15:42:47 -0400

    8/9 8.53 complete

--- accounts/views.py
+++ accounts/views.py
@@ -59,7 +59,11 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, "You are not logged out")
+        return redirect('index')
+
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
