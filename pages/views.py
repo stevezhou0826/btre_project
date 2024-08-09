aaf85ee32fce4665d9c64eb16347572e53c33bb0
stevezhou0826 <zhousihao826@gmail.com>
2024-08-09 17:01:35 -0400

    8/9 get it from brad

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
-from listings.choices import bedroom_choices, price_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
-
-# Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
@@ -19,6 +17,7 @@ def index(request):
 
     return render(request, 'pages/index.html', context)
 
+
 def about(request):
     # Get all realtors
     realtors = Realtor.objects.order_by('-hire_date')
@@ -28,7 +27,7 @@ def about(request):
 
     context = {
         'realtors': realtors,
-        'mvp_realtors': mvp_realtors,
+        'mvp_realtors': mvp_realtors
     }
 
     return render(request, 'pages/about.html', context)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 
 from .models import Realtor
 
-
+@admin.register(Realtor)
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
-    list_display_links = ('id', 'name')
-    search_fields = ('name', )
-    list_per_page  = 25
+  list_display = ('id', 'name', 'email', 'hire_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 25
 
-# Register your models here.
-admin.site.register(Realtor, RealtorAdmin)
